8ef966b075eada636f9089e0d63134fe0fab8864
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-16 19:13:37 +0530

    ReverseString

---
+++ Recursion/ReverseString.java
@@ -0,0 +1,34 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void reverseStringHelper(char[]arr,int start,int end){
+        if(start>=end){
+            return;
+        }
+
+        char temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        reverseStringHelper(arr,start+1, end-1);
+    }
+
+    public static void reverseString(char[]arr){
+        reverseStringHelper(arr, 0, arr.length-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the string: ");
+        String s = sc.nextLine();
+
+        char[] arr = s.toCharArray();
+
+        reverseString(arr);
+        System.out.println("Reversed string is: "+new String(arr));
+
+    }
+}
