d6241bb874253855dcd29b15d592396eb74a9601
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-16 18:47:14 +0530

    Factorial of a number

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class Factorial {
 
     static int factorial(int n){
-        if(n == 1){
+        System.out.println("Calling factorial(" + n + ")");
+
+        // Base case
+        if(n == 0 || n == 1){
+            System.out.println("Returning 1 from factorial(" + n + ")");
             return 1;
         }
-        return n * factorial(n-1);
+
+        int result = n * factorial(n - 1);
+
+        System.out.println("Returning " + result + " from factorial(" + n + ")");
+        return result;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
 
         int result = factorial(n);
-        System.out.println(result);
-
+        System.out.println("Final Answer: " + result);
     }
 }
