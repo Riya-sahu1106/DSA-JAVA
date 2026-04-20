ebd3ef4d663bdafecda37ab0c8a01264c6b566fd
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-20 19:27:35 +0530

    PowerLinear2

--- Recursion/PrintInReverseOrderAlso.java
+++ Recursion/PrintInReverseOrderAlso.java
@@ -13,7 +13,7 @@ public class PrintInReverseOrderAlso{
             if(n==0) return;
             System.out.println(n);
             print(n-1);
-            System.out.println(n);
+            if(n!=1)System.out.println(n);  //for printing 1 at a time
         }
 }
 
