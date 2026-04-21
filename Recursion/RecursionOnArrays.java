276850b8be8486dd47961f0f857b6b7686d71d5c
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-21 16:54:47 +0530

    RecersivelyPrint

---
+++ Recursion/RecursionOnArrays.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class RecursionOnArrays {
+    public static void main(String[] args) {
+        int[]arr= {5,3,7,12,23,4,56,7};
+       recPrint(arr,0);
+    }
+    public static void recPrint(int[]arr,int idx)
+    {
+        if(idx==arr.length)return;
+        System.out.println(arr[idx]);
+        recPrint(arr,idx+1);
+    }
+}
