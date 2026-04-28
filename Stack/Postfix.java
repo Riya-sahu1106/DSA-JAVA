8d024f385d18293d979c208cd1eb4cdea77e23bf
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-28 12:24:04 +0530

    Postfix Evaluation

---
+++ Stack/Postfix.java
@@ -0,0 +1,31 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Postfix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+
+        Stack<Integer> val = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+
+            if (ascii >= 48 && ascii <= 57) {
+                val.push(ascii - 48);
+            }else{
+                int v2 = val.pop();
+                int v1 = val.pop();
+                if(ch =='+') val.push(v1+v2);
+                if(ch =='-') val.push(v1-v2);
+                if(ch =='*') val.push(v1*v2);
+                if(ch =='/') val.push(v1/v2);
+            }
+        }
+        System.out.println(val.peek());
+    }
+}
