deef0f12bcc1c1fc522d744078861e7da747929d
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-04-28 12:39:52 +0530

    PrefixToPostfix

---
+++ Stack/PrefixToPostfix.java
@@ -0,0 +1,28 @@
+package Stack;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PrefixToPostfix {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the String: ");
+            String str = sc.nextLine();
+            Stack<String> val = new Stack<>();
+
+            for (int i = str.length()-1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+
+            if (ascii >= 48 && ascii <= 57) {
+                val.push(ch+"");
+            }else{
+                String v1 = val.pop();
+                String v2 = val.pop();
+                char op = ch;
+                 String t = v1+v2+op;
+                 val.push(t);
+            }
+        }
+        System.out.println(val.peek());
+        }
+    }
