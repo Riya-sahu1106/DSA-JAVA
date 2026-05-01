43e72a4b137c45487a0f3e35a993ebb13af80ed4
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-01 12:08:29 +0530

    Reverse Vowels

--- Stack/CelebrityProblem.java
+++ Stack/CelebrityProblem.java
@@ -6,26 +6,21 @@ public class CelebrityProblem {
 
     static int celebrity(int[][] mat) {
         int n = mat.length;
-
         Stack<Integer> st = new Stack<>();
 
         for (int i = 0; i < n; i++) {
             st.push(i);
         }
-
         while (st.size() > 1) {
             int a = st.pop();
             int b = st.pop();
-
             if (mat[a][b] == 1) {
                 st.push(b);
             } else {
                 st.push(a);
             }
         }
-
         int candidate = st.pop();
-
         for (int i = 0; i < n; i++) {
             if (i != candidate) {
                 if (mat[i][candidate] == 0 || mat[candidate][i] == 1) {
@@ -36,12 +31,10 @@ public class CelebrityProblem {
 
         return candidate;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of people: ");
         int n = sc.nextInt();
-
         int[][] mat = new int[n][n];
         System.out.println("Enter matrix (0 or 1):");
         for (int i = 0; i < n; i++) {
@@ -55,7 +48,6 @@ public class CelebrityProblem {
         } else {
             System.out.println("Celebrity is: " + result);
         }
-
         sc.close();
     }
 }
---
+++ String/ReverseVowels.java
@@ -0,0 +1,49 @@
+package String;
+
+import java.util.Scanner;
+
+public class ReverseVowels {
+
+    public static String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        String vowels = "aeiouAEIOU";
+
+        while (left < right) {
+
+            while (left < right && vowels.indexOf(arr[left]) == -1) {
+                left++;
+            }
+
+            while (left < right && vowels.indexOf(arr[right]) == -1) {
+                right--;
+            }
+
+            // swap vowels
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        String result = reverseVowels(input);
+
+        System.out.println("Reversed Vowels: " + result);
+
+        sc.close();
+    }
+}
