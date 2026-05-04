54fc40d88866fadb2cb15b305ebf565638927676
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-04 20:26:10 +0530

    Previous Smaller Element

---
+++ Stack/PrevSmallerElement.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.*;
+
+public class PrevSmallerElement {
+    public static int[] prevSmallerElement(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
+            }
+            if (st.isEmpty()) {
+                res[i] = -1;
+            } else {
+                res[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arr = {100, 80, 60, 70, 60, 75, 85};
+
+        int[] result = prevSmallerElement(arr);
+
+        for(int ar : arr){
+            System.out.print(ar+" ");
+        }
+        System.out.println();
+        System.out.println("Next Greater Elements:");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+}
+
