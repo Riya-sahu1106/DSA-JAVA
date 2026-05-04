75d0235367d6c7a9642965290ffc4e334cdd3128
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-04 19:26:40 +0530

    Maximum Temperature Using Stack

--- Stack/MaximumTemp2.java
+++ Stack/MaximumTemp2.java
@@ -1,4 +1,31 @@
 package Stack;
+import java.util.*;
 
 public class MaximumTemp2 {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] days = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                days[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {75, 71, 69, 72};
+
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.println("Input: " + Arrays.toString(temperatures));
+        System.out.println("Output: " + Arrays.toString(result));
+    }
 }
