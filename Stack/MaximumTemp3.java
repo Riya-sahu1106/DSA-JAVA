d1a1f640b4d0f92ced969c5295cc5c9ba34e99bf
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-04 19:29:02 +0530

    Optimized solution for Daily Temperatures using DP jump approach

--- Stack/MaximumTemp3.java
+++ Stack/MaximumTemp3.java
@@ -1,7 +1,42 @@
 package Stack;
+import java.util.Arrays;
 
 public class MaximumTemp3 {
-    public static void main(String[] args) {
 
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] days = new int[n];
+
+        int warmest = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int currTemp = temperatures[i];
+
+            if (currTemp >= warmest) {
+                warmest = currTemp;
+                continue;
+            }
+
+            int count = 1;
+
+            while (true) {
+                if (temperatures[i + count] > currTemp) {
+                    days[i] = count;
+                    break;
+                } else {
+                    count = count + days[i + count];
+                }
+            }
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {75, 71, 69, 72};
+
+        int[] result = dailyTemperatures(temperatures);
+
+        System.out.println("Input: " + Arrays.toString(temperatures));
+        System.out.println("Output: " + Arrays.toString(result));
     }
 }
