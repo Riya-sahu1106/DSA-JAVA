f23b612ca1e1f901f2a46076f30fd6297b317d9a
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-06 04:35:45 +0530

    Implement 90-degree matrix rotation using transpose and row reversal

---
+++ Array/RotateImage.java
@@ -0,0 +1,54 @@
+package Array;
+
+import java.util.*;
+
+public class RotateImage {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n (size of matrix): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        rotate(matrix);
+
+        System.out.println("Rotated Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ Array/ValidSudoku.java
@@ -0,0 +1,7 @@
+package Array;
+
+public class ValidSudoku {
+    public static void main(String[] args) {
+
+    }
+}
