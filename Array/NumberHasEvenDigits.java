1731e1f828eb25a28ba3618a7ba46f12f7a6d866
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-06 05:00:11 +0530

    Add solution to count numbers with even number of digits

---
+++ Array/NumberHasEvenDigits.java
@@ -0,0 +1,50 @@
+package Array;
+import java.util.*;
+public class NumberHasEvenDigits {
+
+    public static boolean numberHasEvenDigits(int num) {
+            int digitCount = 0;
+
+            if (num == 0) return false;
+
+            while (num != 0) {
+                num = num / 10;
+                digitCount++;
+            }
+
+            return digitCount % 2 == 0;
+        }
+
+        public static int findNumbers(int[] nums) {
+            int evenCount = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (numberHasEvenDigits(nums[i])) {
+                    evenCount++;
+                }
+            }
+
+            return evenCount;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter size of array: ");
+            int n = sc.nextInt();
+
+            int[] nums = new int[n];
+
+            System.out.println("Enter elements:");
+            for (int i = 0; i < n; i++) {
+                nums[i] = sc.nextInt();
+            }
+
+            int result = findNumbers(nums);
+
+            System.out.println("Count of numbers with even digits: " + result);
+
+            sc.close();
+        }
+    }
+
