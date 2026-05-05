8528ec86c619046588eb13a516f27ef41a58719d
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-05-05 08:27:11 +0530

    Implement Intersection of Two Arrays II using sorting and two-pointer technique

--- Array/IntersectionOfTwoArrays.java
+++ Array/IntersectionOfTwoArrays.java
@@ -8,23 +8,26 @@ import java.util.Scanner;
 public class IntersectionOfTwoArrays {
 
     public static int[] intersection(int[] nums1, int[] nums2){
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num:nums1){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
         int[]res = new int[nums1.length];
         int k = 0;
 
-        for(int num : nums2){
-            int count = map.getOrDefault(num,0);
-
-            if(count == 0){
-                continue;
-            }else{
-                res[k++] = num;
-                map.put(num,count-1);
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i] == nums2[j]){
+                res[k++] = nums1[i++];
+                j++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                i++;
             }
         }
+
         return Arrays.copyOfRange(res,0,k);
     }
     public static void main(String[] args) {
