410ea47fdc5b1aa9e4f28e8dc9e615792365971d
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-07-08 15:18:34 +0530

    Add program to find GCD or HCF of two numbers

---
+++ NumberBasedQuestions/FindGCD.java
@@ -0,0 +1,26 @@
+package NumberBasedQuestions;
+
+import java.util.Scanner;
+
+public class FindGCD {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        while (b != 0) {
+
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+
+        System.out.println("GCD/HCF: " + a);
+    }
+}
