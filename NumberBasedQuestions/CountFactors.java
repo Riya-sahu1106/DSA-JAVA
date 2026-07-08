19f12e1c65f975ce80a7c912601000e3bc6cff24
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-07-08 18:50:02 +0530

    Add program to count factors of a number

---
+++ NumberBasedQuestions/CountFactors.java
@@ -0,0 +1,25 @@
+package NumberBasedQuestions;
+
+import java.util.Scanner;
+
+public class CountFactors {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of factors: " + count);
+    }
+}
