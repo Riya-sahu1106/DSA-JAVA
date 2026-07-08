977f2732107c0b0130bb9bab42c8ef79e9d59e39
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-07-08 18:47:45 +0530

    git commit -m "Add program to find all factors of a number"

---
+++ NumberBasedQuestions/FindAllFactors.java
@@ -0,0 +1,23 @@
+package NumberBasedQuestions;
+
+import java.util.Scanner;
+
+public class FindAllFactors {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Factors are: ");
+
+        for (int i = 1; i <= n; i++) {
+
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
