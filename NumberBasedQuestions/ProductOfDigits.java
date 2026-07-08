af159166e06f111cc9432a6fc2a495d4d078843c
Riya_sahu1106 <riyasahu885542@gmail.com>
2026-07-08 12:22:21 +0530

    Add program to calculate product of digits

---
+++ NumberBasedQuestions/ProductOfDigits.java
@@ -0,0 +1,25 @@
+package NumberBasedQuestions;
+
+import java.util.Scanner;
+
+public class ProductOfDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int product = 1;
+
+        while (n > 0) {
+
+            int digit = n % 10;
+            product = product * digit;
+            n = n / 10;
+        }
+
+        System.out.println("product of digits: " + product);
+    }
+}
